06defeceed523a19f5d8df48c9ce18db56a399bc
Hongyu.Zhao <nathan_115210@hotmail.com>
2025-07-10 00:22:47 +0300

    Remove JS files in styles/core from version control

--- styles/core/borderRadius.js
+++
@@ -1,9 +0,0 @@
-export const borderRadius = {
-    none: '0px',
-    sm: '2px',
-    md: '4px',
-    lg: '8px',
-    xl: '16px',
-    pill: '9999px',
-    circle: '50%',
-};
--- styles/core/breakpoints.js
+++
@@ -1,8 +0,0 @@
-export const breakpoints = {
-    xs: '480px',
-    sm: '600px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    xxl: '1536px',
-};
